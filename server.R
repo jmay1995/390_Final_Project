1a1f2705724ac4d4b2ac3a719487cf455c541b69
ahariharan95 <harihara@live.unc.edu>
2017-04-27 17:27:01 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -15,14 +15,17 @@ library(dplyr)
 
 
 
-#points_data <- map_debuts[order(map_debuts$location),]
 
 # Define server logic required to draw a map
 server <- function(input, output, session) {
+    # map_world = map("world", fill = TRUE, plot = FALSE)
+    # map_world <- as.data.frame(map_world)
+    # map_world
+    
     phanalytix_map <- read_csv('phanalytix.csv')
     
     
-    test <- phanalytix_map %>% group_by(venue_name, latitude, longitude) %>% summarize(num_perform = n()) 
+    test <- phanalytix_map %>% group_by(venue_name, latitude, longitude, city, state, country) %>% summarize(num_perform = n()) 
     
     #test2 <- right_join(test, phanalytix_map, by="venue_name") %>% select(venue_name, num_perform, latitude, longitude, song)
     
@@ -30,26 +33,26 @@ server <- function(input, output, session) {
    
     output$map <- renderLeaflet({
         
-       # mapStates = map("state", fill = TRUE, plot = FALSE)
         leaflet(test) %>% addTiles() %>%
-            addMarkers( ~longitude, ~latitude, popup = paste("Number of performances: ", as.character(test$num_perform), 
-                                                              as.character(test$venue_name)) )
+            addMarkers( ~longitude, ~latitude, popup = paste("Number of songs performed at: ", 
+                                                             as.character(test$venue_name), 
+                                                             "=",  
+                                                             as.character(test$num_perform)) )
            # addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
         
     })
     
-    # A reactive expression that returns the set of zips that are
-    # in bounds right now
-    points_in_bounds <- reactive({
-        if (is.null(input$map_bounds))
-            return(points_data[FALSE,])
-        bounds <- input$map_bounds
-        latRng <- range(bounds$north, bounds$south)
-        lngRng <- range(bounds$east, bounds$west)
-        
-        subset(points_data,
-               latitude >= latRng[1] & latitude <= latRng[2] &
-                   longitude >= lngRng[1] & longitude <= lngRng[2])
-        
-    })
+    # 
+    # points_in_bounds <- reactive({
+    #     if (is.null(input$map_bounds))
+    #         return(points_data[FALSE,])
+    #     bounds <- input$map_bounds
+    #     latRng <- range(bounds$north, bounds$south)
+    #     lngRng <- range(bounds$east, bounds$west)
+    #     
+    #     subset(points_data,
+    #            latitude >= latRng[1] & latitude <= latRng[2] &
+    #                longitude >= lngRng[1] & longitude <= lngRng[2])
+    #     
+    #})
 }
